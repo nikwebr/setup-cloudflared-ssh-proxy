793f2a55c78fbf7e1290d9f4525bf04ab87deb6b
Niklas Weber <41891479+nikwebr@users.noreply.github.com>
2023-04-21 23:17:42 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 export HOST=$1
 export DOLLAR="$"
-export CF_CLIENT_ID=$4
-export CF_CLIENT_SECRET=$5
+export CF_CLIENT_ID=$2
+export CF_CLIENT_SECRET=$3
 
 envsubst < "/root/ssh-client.conf" > "/root/.ssh/config"
