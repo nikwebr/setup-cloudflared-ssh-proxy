f252c4311d5f6047539a35cc0451c55cd393369b
Niklas Weber <41891479+nikwebr@users.noreply.github.com>
2023-04-21 23:12:45 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,20 +7,4 @@ export DOLLAR="$"
 export CLIENT_ID=$4
 export CLIENT_SECRET=$5
 
-envsubst < "/root/ssh-client.conf" > "/root/.ssh/config" 
-envsubst < "/root/expect.sh" > "/root/expect-final.sh" 
-PUBLIC_KEY="$(echo $6 | base64 --decode)"
-PRIVATE_KEY="$(echo $7 | base64 --decode)"
-sh -c "cat > /root/.ssh/id_rsa << 'EOL'
-$PRIVATE_KEY
-EOL"
-sh -c "cat > /root/.ssh/id_rsa.pub << 'EOL'
-$PUBLIC_KEY
-EOL"
-
-chmod 0600 /root/.ssh/id_rsa
-echo "***  connecting to ssh targrt: ***\n"
-cat /root/.ssh/config
-echo "***  running commands: ***\n"
-cat ./commands.txt
-/root/expect-final.sh
+envsubst < "/root/ssh-client.conf" > "/root/.ssh/config"
